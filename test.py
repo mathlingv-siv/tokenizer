5c6969345053f07bf4b67d62705aee25ef507029
mathlingv-siv <43806310+mathlingv-siv@users.noreply.github.com>
2018-10-09 20:54:40 +0300

    Update test.py

--- test.py
+++ test.py
@@ -143,6 +143,75 @@ class TestTokenizer(unittest.TestCase):
         self.assertEqual(text[0].position, 3)
         self.assertEqual(text[1].string, 'And')
         self.assertEqual(text[1].position, 9)
+       
+    def test_type_string(self):
+        """test if the program assigns the right type
+        to alphabetic characters
+
+        """
+        text = list(self.t.type_aware_tokenize('and'))
+        self.assertEqual(text[0].type, 'a')
+
+    def test_type_digit(self):
+        """test if the program assigns the right type
+        to numeric characters
+
+        """        
+        text = list(self.t.type_aware_tokenize('123'))
+        self.assertEqual(text[0].type, 'd')
+
+    def test_type_space(self):
+        """test if the program assigns the right type
+        to spaces
+
+        """        
+        text = list(self.t.type_aware_tokenize('k p'))
+        self.assertEqual(text[1].type, 's')
+
+    def test_type_punc(self):
+        """test if the program assigns the right type
+        to punctuation marks
+
+        """        
+        text = list(self.t.type_aware_tokenize('k, p'))
+        self.assertEqual(text[1].type, 'p')
+
+    def test_type_other(self):
+        """test if the program assigns the right type
+        to other kinds of characters
+
+        """
+        text = list(self.t.type_aware_tokenize('+'))
+        self.assertEqual(text[0].type, 'o')
+
+
+    def test_type_aware(self):
+        """test if the type aware tokenizer is working correctly
+    
+        """
+        text = list(self.t.type_aware_tokenize('...test. And...'))
+        self.assertEqual(len(text), 6)
+        self.assertEqual(text[0].string, '...')
+        self.assertEqual(text[0].position, 0)
+        self.assertEqual(text[1].string, 'test')
+        self.assertEqual(text[1].position, 3)
+        self.assertEqual(text[2].string, '.')
+        self.assertEqual(text[2].position, 7)
+        self.assertEqual(text[5].string, '...')
+        self.assertEqual(text[5].position, 12)
+        
+    def test_type_aware_non_string(self):
+        """test if the programm is working correctly
+        when tokenizing a non-string object or an empty string    
+
+        """
+        with self.assertRaises(TypeError):
+            list(self.t.type_aware_tokenize(789))
+        with self.assertRaises(TypeError):
+            list(self.t.type_aware_tokenize(''))
+        array = [25, 'array', '79 - 0']
+        with self.assertRaises(TypeError):
+            list(self.t.type_aware_tokenize(array))
 
 
 if __name__=='__main__':
