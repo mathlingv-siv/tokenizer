f35287bdcca6c16f9c4e1caccb92f2fb3da9c74c
mathlingv-siv <43806310+mathlingv-siv@users.noreply.github.com>
2019-06-19 15:05:00 +0300

    Add files via upload

---
+++ web_server.py
@@ -0,0 +1,31 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        """create an html page with a field and a search button
+
+        """
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+        html = """
+                <html>
+                    <body>
+                        <form method="post">
+                            <input type="text" name="query">
+                            <input type="submit" value="Search">
+                        </form>
+                    </body>
+                </html>
+                """
+        self.wfile.write(bytes(html, encoding="UTF-8"))
+
+
+def main():
+    server = HTTPServer(('', 80), RequestHandler)
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
